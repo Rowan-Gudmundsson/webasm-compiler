e7810f66d81c1ce516e063ffd694cdbf7140b5a6
Rowan Gudmundsson <rgudmundsson@nevada.unr.edu>
2019-01-04 20:11:02 -0800

    remove build folder

--- build/CMakeFiles/Webasm_SUCCESSFUL.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Webasm_SUCCESSFUL"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Webasm_SUCCESSFUL.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
